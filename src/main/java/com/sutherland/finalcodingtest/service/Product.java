f1546ae4ffcff0dee147caba1f43be56d435a606
s4tyam <satyam9128532410@gmail.com>
2025-09-01 11:01:34 +0530

    done

--- src/main/java/com/sutherland/finalcodingtest/service/Product.java
+++ src/main/java/com/sutherland/finalcodingtest/service/Product.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Product {
 	private int productId;
 	private String productName;
